75435ff7f95603688bc9c61f3313cdadeea068a9
AndrewRiabets <Inferion5@gmail.com>
2021-08-19 19:29:54 +0300

    hw

---
+++ src/get-refs.js
@@ -0,0 +1,6 @@
+export default function getRefs () {
+    return {
+      cardContainer: document.querySelector('.country-card-container'),
+      searchForm: document.querySelector('.js-search-form'),
+    };
+}
